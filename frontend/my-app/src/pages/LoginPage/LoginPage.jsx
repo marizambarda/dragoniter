329d3d4e5319592f6dade2043ef0e5b23dc4a95b
Mariana Zambarda <marianazambarda@gmail.com>
2020-11-09 16:22:40 -0300

    implementação carregando pagina de cadastro e pagina de login

--- frontend/my-app/src/pages/LoginPage/LoginPage.jsx
+++ frontend/my-app/src/pages/LoginPage/LoginPage.jsx
@@ -3,20 +3,23 @@ import api from "../../api";
 import { Link, useHistory } from "react-router-dom";
 import { Form, Button, Container } from "react-bootstrap";
 import "./LoginPage.scss";
+import LoadingIndicator from "../../components/LoadingIndicator";
 
 function LoginPage({setIsLoggedIn}){
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
 
   async function formSubmitted(e){
     e.preventDefault()
-
+    setIsLoading(true)
     const response = await api.post(`/users/login`, {
       email: email,
       password: password
     })
+    setIsLoading(false)
 
     if (response.ok) {
       localStorage.setItem("access_token", response.data.access_token)
@@ -31,31 +34,36 @@ function LoginPage({setIsLoggedIn}){
 
   return(
     <Container>
-    <h1>Entrar</h1>
-    <Form className="form"  onSubmit={formSubmitted} 
-    >
-      <Form.Group controlId="itemName">
-        <Form.Label>Email</Form.Label>
-        <Form.Control 
-          type="email" 
-          placeholder="Digite o email"
-          value={email} 
-          onChange={ e => setEmail(e.target.value) }
-        />
-        <Form.Label>Senha</Form.Label>
-        <Form.Control 
-          type="password" 
-          placeholder="Digite a senha" 
-          value={ password } 
-          onChange={ e => setPassword(e.target.value) }
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Entrar
-      </Button><br/><br/>
-      <Link to="/users/signup">Não tem uma conta? Click aqui</Link>
-    </Form>
-  </Container>
+      {isLoading && <LoadingIndicator/>}
+      {!isLoading && (
+        <div>
+          <h1>Entrar</h1>
+          <Form className="form"  onSubmit={formSubmitted} 
+          >
+            <Form.Group controlId="itemName">
+              <Form.Label>Email</Form.Label>
+              <Form.Control 
+                type="email" 
+                placeholder="Digite o email"
+                value={email} 
+                onChange={ e => setEmail(e.target.value) }
+              />
+              <Form.Label>Senha</Form.Label>
+              <Form.Control 
+                type="password" 
+                placeholder="Digite a senha" 
+                value={ password } 
+                onChange={ e => setPassword(e.target.value) }
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Entrar
+            </Button><br/><br/>
+            <Link to="/users/signup">Não tem uma conta? Click aqui</Link>
+          </Form>
+        </div>
+      )}
+    </Container>
   )
 }
 
--- frontend/my-app/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend/my-app/src/pages/SignUpPage/SignUpPage.jsx
@@ -3,6 +3,7 @@ import api from "../../api";
 import { Link, useHistory } from "react-router-dom";
 import { Form, Button, Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
 import "./SignUpPage.scss";
+import LoadingIndicator from "../../components/LoadingIndicator";
 
 function SignUpPage({setIsLoggedIn}){
   const history = useHistory();
@@ -10,17 +11,19 @@ function SignUpPage({setIsLoggedIn}){
   const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
 
   async function formSubmitted(e){
     e.preventDefault()
-
+    setIsLoading(true)
     const response = await api.post(`/users`, {
       name: name,
       nickname: nickname,
       email: email,
       password: password
     })
+    setIsLoading(false)
 
     if (response.ok) {
       localStorage.setItem("access_token", response.data.access_token)
@@ -32,63 +35,68 @@ function SignUpPage({setIsLoggedIn}){
   }
   return(
     <Container>
-      <h1>Cadastrar-se</h1>
-      <Form className="form" onSubmit={formSubmitted}
-      >
-        <Form.Group controlId="itemName">
-        <Row> 
-          <Col>
-            <Form.Label>Nome</Form.Label>
-            <Form.Control 
-              type="name" 
-              placeholder="Digite o nome"
-              value={name}
-              onChange={ e => setName(e.target.value) }
-            />
-          </Col>
-          <Col>
-            <Form.Label>
-              Nome de Usuário
-            </Form.Label>
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text>@</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl id="inlineFormInputGroupUsername" 
-              placeholder="Digite o nome de usuário" 
-              value={nickname}
-              onChange={ e => setNickname(e.target.value) }/>
-            </InputGroup>
-          </Col>
-        </Row>
-       
-        <Row>
-          <Col>
-          <Form.Label>Email</Form.Label>
-          <Form.Control 
-            type="email" 
-            placeholder="Digite o email"
-            value={email}
-            onChange={ e => setEmail(e.target.value) }
-          />
-          </Col>
-          <Col> 
-          <Form.Label>Senha</Form.Label>
-          <Form.Control 
-            type="password" 
-            placeholder="Digite a senha"  
-            value={password}
-            onChange={ e => setPassword(e.target.value) }
-          />
-          </Col>
-        </Row>
+      {isLoading && <LoadingIndicator/>}
+      {!isLoading && (
+        <div>
+          <h1>Cadastrar-se</h1>
+          <Form className="form" onSubmit={formSubmitted}
+          >
+            <Form.Group controlId="itemName">
+            <Row> 
+              <Col>
+                <Form.Label>Nome</Form.Label>
+                <Form.Control 
+                  type="name" 
+                  placeholder="Digite o nome"
+                  value={name}
+                  onChange={ e => setName(e.target.value) }
+                />
+              </Col>
+              <Col>
+                <Form.Label>
+                  Nome de Usuário
+                </Form.Label>
+                <InputGroup>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>@</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl id="inlineFormInputGroupUsername" 
+                  placeholder="Digite o nome de usuário" 
+                  value={nickname}
+                  onChange={ e => setNickname(e.target.value) }/>
+                </InputGroup>
+              </Col>
+            </Row>
           
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Entrar
-        </Button><br/><br/>
-        <Link to="/users/login">Já tem uma conta? Click aqui</Link>
-      </Form>
+            <Row>
+              <Col>
+              <Form.Label>Email</Form.Label>
+              <Form.Control 
+                type="email" 
+                placeholder="Digite o email"
+                value={email}
+                onChange={ e => setEmail(e.target.value) }
+              />
+              </Col>
+              <Col> 
+              <Form.Label>Senha</Form.Label>
+              <Form.Control 
+                type="password" 
+                placeholder="Digite a senha"  
+                value={password}
+                onChange={ e => setPassword(e.target.value) }
+              />
+              </Col>
+            </Row>
+              
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Entrar
+            </Button><br/><br/>
+            <Link to="/users/login">Já tem uma conta? Click aqui</Link>
+          </Form>
+        </div>
+      )}
     </Container>
   )
 }
