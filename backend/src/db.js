315b518db100415f53da3342c9492d2e5bd50036
Mariana Zambarda <marianazambarda@gmail.com>
2020-12-04 18:18:47 -0300

    configuracoes de conexao do banco de dados extraidas para variaveis de ambiente

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,22 @@
 const mysql = require('mysql2');
+const { ConnectionString } = require('connection-string');
 
-const con = mysql.createConnection({
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'twitter',
-});
+const obj = new ConnectionString(
+  process.env.CLEARDB_DATABASE_URL || process.env.DATABASE_URL
+);
+
+const config = {
+  host: obj.host,
+  user: obj.user,
+  password: obj.password,
+  database: obj.path.toString()
+};
+
+if (process.env.DATABASE_SOCKET_PATH) {
+  config.socketPath = process.env.DATABASE_SOCKET_PATH;
+}
+
+const con = mysql.createConnection(config);
 
 con.connect(function (err) {
   if (err) throw err;
